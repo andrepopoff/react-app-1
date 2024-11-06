2eba492fb96f0e082a5f3c9577cd57333c9eda7f
Andrei Popov <andrepopoff@gmail.com>
2024-11-06 10:27:40 +0300

    Refactor

--- src/components/Stars.tsx
+++ src/components/Stars.tsx
@@ -1,17 +1,17 @@
 import Star from "./Star";
 
 type ComponentProps = {
-    count: 0 | 1 | 2 | 3 | 4 | 5,
-}
+  count: 0 | 1 | 2 | 3 | 4 | 5;
+};
 
 export default function Stars({ count = 0 }: ComponentProps) {
-    return (
-        <>
-            {count &&
-              <ul className="card-body-stars u-clearfix">
-                  {new Array(count).fill(<Star />)}
-              </ul>
-            }
-        </>
-    )
+  return (
+    <>
+      {count && (
+        <ul className="card-body-stars u-clearfix">
+          {new Array(count).fill(<Star />)}
+        </ul>
+      )}
+    </>
+  );
 }
